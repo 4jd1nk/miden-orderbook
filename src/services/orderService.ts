eb956898e21126ff7e1f8a43bf056d8681de535e
ajdin-kahrovic1 <kahrovic.ajdin@gmail.com>
2023-07-22 20:35:44 +0200

    Improving mock prove_program

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -62,7 +62,9 @@ export async function GetOrders() {
 }
 
 function prove_program_mock(input:string) {
-    const outputStack = new BigUint64Array(18);
+    //modification, deletion and addition
+    //modification
+    const outputStack = new BigUint64Array(45);
     outputStack[0] = 268n;    //memoryLocation
     outputStack[1] = 1n;      //color = red (changed)
     outputStack[2] = 259n;    //(parentId)
@@ -73,15 +75,49 @@ function prove_program_mock(input:string) {
     outputStack[7] = 1688473018n;  //timestamp
     outputStack[8] = 4929032n;     //orderId
 
-    outputStack[9] = 241n;    //memoryLocation
-    outputStack[10] = 1n;      //color = red 
-    outputStack[11] = 217n;    //(parentId)
-    outputStack[12] = 265n;      //leftChildId
-    outputStack[13] = 229n;      //rightChildId
-    outputStack[14] = 14n;      //quantity (changed)
+    //deleted node
+    outputStack[9] = 274n;    //memoryLocation
+    outputStack[10] = 0n;      //color = black
+    outputStack[11] = 0n;    //(parentId)
+    outputStack[12] = 0n;      //leftChildId
+    outputStack[13] = 0n;      //rightChildId
+    outputStack[14] = 0n;      //quantity
     outputStack[15] = 48n;     //price
-    outputStack[16] = 1688474494n;  //timestamp
-    outputStack[17] = 2451248n;     //orderId
+    outputStack[16] = 1688473234n;  //timestamp
+    outputStack[17] = 3248575n;     //orderId
+
+    //parent of deleted node
+    outputStack[18] = 271n;    //memoryLocation
+    outputStack[19] = 0n;      //color = black
+    outputStack[20] = 277n;    //(parentId)
+    outputStack[21] = 280n;      //leftChildId
+    outputStack[22] = 0n;      //rightChildId
+    outputStack[23] = 40n;      //quantity
+    outputStack[24] = 49n;     //price
+    outputStack[25] = 1688473214n;  //timestamp
+    outputStack[26] = 2697766n;     //orderId   
+
+    //addition
+    outputStack[27] = 300n;    //memoryLocation
+    outputStack[28] = 1n;      //color = red 
+    outputStack[29] = 286n;    //(parentId)
+    outputStack[30] = 0n;      //leftChildId
+    outputStack[31] = 0n;      //rightChildId
+    outputStack[32] = 25n;      //quantity (changed)
+    outputStack[33] = 49n;     //price
+    outputStack[34] = 1688473018n;  //timestamp
+    outputStack[35] = 4929032n;     //orderId
+
+    //parent of added node
+    outputStack[36] = 286n;    //memoryLocation
+    outputStack[37] = 0n;      //color = black 
+    outputStack[38] = 283n;    //(parentId)
+    outputStack[39] = 300n;      //leftChildId
+    outputStack[40] = 0n;      //rightChildId
+    outputStack[41] = 5n;      //quantity (changed)
+    outputStack[42] = 49n;     //price
+    outputStack[43] = 1688474789n;  //timestamp
+    outputStack[44] = 4306835n;     //orderId
 
     const proof = new Uint8Array(3);
     proof[0] = 200;
@@ -128,7 +164,6 @@ export async function createOrder(quantity: number, price: number | null, side :
 
     await init();
  
-
     inputData.operand_stack[0] = side.toString();
     inputData.operand_stack[1] = quantity.toString();
     let sPrice : string;
@@ -216,6 +251,9 @@ export async function createOrder(quantity: number, price: number | null, side :
 
         i++;
     }
+
+    newAdviceMap[keyPrefix+keySuffix] = [0, 0, 0, i-1];
+    inputData.advice_map = newAdviceMap;
 
     console.log(uiTree);
     console.log(inputData);
