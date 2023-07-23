951c9f89b0991cce5152a68f34261e8680ad5483
ajdin-kahrovic1 <kahrovic.ajdin@gmail.com>
2023-07-23 08:14:16 +0200

    Adding UI

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -6,8 +6,7 @@ export const nodes = [
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
-	() => import('./nodes/5'),
-	() => import('./nodes/6')
+	() => import('./nodes/5')
 ];
 
 export const server_loads = [];
@@ -15,9 +14,8 @@ export const server_loads = [];
 export const dictionary = {
 		"/": [2],
 		"/ob": [3],
-		"/orders": [4],
-		"/playground": [5],
-		"/wasm": [6]
+		"/playground": [4],
+		"/wasm": [5]
 	};
 
 export const hooks = {
